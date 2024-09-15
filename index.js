96ec661db280e67a138c309af7874712e52efcf0
Whiteshark2 <rahulsonu2279@gmail.com>
2024-09-15 11:40:41 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('express')
+const app=express()
+
+
+
+
+
+
+
+
+const port=process.env.PORT
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running server at port : ${port}`)
+    }
+    console.log(`Server is running at port : ${port}`)
+})
